1722975d2995872090da1e1557adbe5b6e3d7856
alexlefk <64595001+alexlefk@users.noreply.github.com>
2020-05-20 13:51:25 +0300

    deployment

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/smart-eye/precache-manifest.dc723613b0ac8e411fe47c27fad7daed.js"
+  "/smart-eye/precache-manifest.75f71b0ad18afdb1829610a7b84169c6.js"
 );
 
 self.addEventListener('message', (event) => {
