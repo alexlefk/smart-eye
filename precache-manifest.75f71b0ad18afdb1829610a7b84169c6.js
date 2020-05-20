0c8d2be83e73ef3e744c28e28d9f9c849c736c02
alexlefk <64595001+alexlefk@users.noreply.github.com>
2020-05-20 13:53:51 +0300

    Updates

---
+++ precache-manifest.75f71b0ad18afdb1829610a7b84169c6.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "82f35e538348e91aa3aeb89d7e632dda",
+    "url": "/smart-eye/index.html"
+  },
+  {
+    "revision": "6699a45caa111e7882cc",
+    "url": "/smart-eye/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "93dc4cfb9d949f3956e0",
+    "url": "/smart-eye/static/css/main.35a3fe01.chunk.css"
+  },
+  {
+    "revision": "6699a45caa111e7882cc",
+    "url": "/smart-eye/static/js/2.94b7c5a0.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/smart-eye/static/js/2.94b7c5a0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "93dc4cfb9d949f3956e0",
+    "url": "/smart-eye/static/js/main.8c51b44a.chunk.js"
+  },
+  {
+    "revision": "57169b437331250c1d42",
+    "url": "/smart-eye/static/js/runtime-main.e643a0bc.js"
+  },
+  {
+    "revision": "3da0c51c2b0f5992b94727d0b497abff",
+    "url": "/smart-eye/static/media/eye.3da0c51c.gif"
+  }
+]);
